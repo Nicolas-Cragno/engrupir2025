d3acc480d6a0fb741ab97523ab3826ca79b7aa6e
NicolasCragno <nicolaslautaro.cragno@gmail.com>
2025-05-19 21:30:45 -0300

    asfsa

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import "../assets/css/Card.css";
+import { Link } from "react-router-dom";
 
-export default function Card() {
+export default function Card(props) {
+  const { img, title, txt, link } = props;
+  const cardStyle = {
+    backgroundImage: `url(${img})`,
+    backgroundSize: 'cover',
+  };
   return (
-    <div>
-      
-    </div>
+      <Link to="/" className="link">
+        <div className='card' style={cardStyle}>
+            <div className="card-content">
+              <h2>{title}</h2>
+              <p>{txt}</p>  
+            </div>
+        </div>
+      </Link>
   )
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,19 +1,51 @@
-import { Link } from "react-router-dom";
 import "../assets/css/Home.css";
 import Linkto from "../components/Linkto";
 import LogoSoundcloud from "../assets/logos/soundCloudOrange.avif";
+import AcidGrooveImg1 from "../assets/images/carajo.jpg";
+import AcidGrooveImg2 from "../assets/images/acidhouse.jpg";
+import VanguartImg1 from "../assets/images/ophelia.jpg";
+import VanguartImg2 from "../assets/images/toto.jpg";
+import Card from "../components/Card";
 
 export default function Home() {
   return (
     <div className="home">
       <div className="presentation"></div>
-      <div className="soundcloud-section">
+      <div className="soundcloud section">
         <h2>Entrevista</h2>
         <Linkto 
         link="https://soundcloud.com/brii-martinez/vertigo-entrevista?ref=clipboard&p=a&c=1&si=c6878d736a664abda1119002e352f8e1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
         title="Charlamos con Diego Lugano, dueño de Vértigo."
         img={LogoSoundcloud}
         />
+      </div>
+      <div className="notes section">
+          <Card 
+          img={AcidGrooveImg1} 
+          title='Sin anestesia' 
+          txt='Repasamos la trayectoria de Carajo, desde su nacimiento en plena crisis hasta la actualidad.' 
+          link='/'
+          ></Card>
+          <Card 
+          img={AcidGrooveImg2}
+          title='Acid House'
+          txt='Conoce la historia de este movimiento under surgido en los 80s.'
+          link='/'
+          ></Card>
+      </div>
+            <div className="notes section">
+          <Card 
+          img={VanguartImg1} 
+          title='Ophelia Liu Mua' 
+          txt='Conoce la obra de esta artista oriunda de Hong Kong.' 
+          link='/'
+          ></Card>
+          <Card 
+          img={VanguartImg2}
+          title='Toto Tatuer'
+          txt='De tatuar pieles a tallar vidrios. Te mostramos la obra de este gran artista argentino.'
+          link='/'
+          ></Card>
       </div>
     </div>
   )
